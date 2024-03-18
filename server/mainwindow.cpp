bc7e79723d2f155f289879fcceb508166bd293e3
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2024-03-17 23:49:57 -0600

    UI Update

--- server/mainwindow.cpp
+++ server/mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QPushButton>
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+        : QMainWindow(parent)
+        , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    playBtn = ui->playBtn;
+
+    connect(playBtn, SIGNAL(clicked()), this, SLOT(handlePlayBtn()));
+}
+
+void MainWindow::handlePlayBtn() {
+    std::cout << "Play" << std::endl;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
--- server/mainwindow.h
+++ server/mainwindow.h
@@ -2,22 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    static void handlePlayBtn() ;
+
 private:
     Ui::MainWindow *ui;
+    QPushButton* playBtn;
 };
+
 #endif // MAINWINDOW_H
+
